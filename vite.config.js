be3fa9473df33481bb805d48d9fad3afce0278f8
CJ <hljchenjun@live.com>
2026-04-30 14:44:53 +0800

    Use relative base path for flexible deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/echarts-playground/',
+  base: './', // 使用相对路径，适配任何部署环境
   server: {
     port: 3000,
     open: true
